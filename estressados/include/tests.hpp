4b1911aba1e981149c29d5f739e4c3c3ea27bc46
hugorplobo <hugorplobo@gmail.com>
2021-10-05 18:56:34 +0000

    Add tests runner

---
+++ estressados/include/tests.hpp
@@ -0,0 +1,23 @@
+#ifndef SIMPLE_TEST_HPP
+#define SIMPLE_TEST_HPP
+
+#include <iostream>
+#include <vector>
+
+template <typename T>
+void testar(const T& A, const T& B) {
+    if (A == B)
+        std::cout << "\tOk\n";
+    else
+        std::cout << A << " != " << B << "\n";
+}
+
+std::ostream& operator<<(std::ostream& os, const std::vector<int>& v) {
+    os << "[ ";
+    for (auto i : v)
+        os << i << " ";
+    os << "]";
+    return os;
+}
+
+#endif
