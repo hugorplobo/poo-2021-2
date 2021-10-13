0854b8c84298e94f7d047647156405741de7b428
hugorplobo <hugorplobo@gmail.com>
2021-10-13 20:32:41 +0000

    Update random ganeration and add tests

--- estressados/main.cpp
+++ estressados/main.cpp
@@ -1,11 +1,18 @@
 #include <iostream>
 #include <vector>
+#include <cstdlib>
+#include <ctime>
 #include "include/busca.hpp"
 #include "include/melhor_caso.hpp"
 #include "include/contagem.hpp"
 #include "include/filter.hpp"
 #include "include/acesso.hpp"
 #include "include/conjuntos.hpp"
+#include "include/manipulacao.hpp"
+#include "include/proximidade.hpp"
+#include "include/sequencias.hpp"
+#include "include/grupos.hpp"
+#include "include/alteracao.hpp"
 #include "include/tests.hpp"
 
 using namespace std;
@@ -66,11 +73,51 @@ void testarConjuntos() {
     testar(abandonados({5, 3, -1, -50, -1, -99}), {-1});
 }
 
+void testarManipulacao() {
+    cout << "\nTestes de manipulação: \n";
+    testar(sozinhos({1, 3, 4, 3, -1, -3, -3}), {4});
+    testar(maisOcorrencias({1, 3, 4, 5, -1, -5, -5}), 3);
+    testar(maisRecorrentes({1, 3, 4, 5, -1, -5, -5, 3, -3}), {3, 5});
+}
+
+void testarProximidade() {
+    cout << "\nTestes de proximidade: \n";
+    testar(briga({-89, 34, -90, 99, 87, 10}), 2);
+    testar(apaziguados({89, -8, -4, -95, 99, 45}), {0, 3});
+}
+
+void testarSequencias() {
+    cout << "\nTestes de sequências: \n";
+    testar(quantosTimes({2, 99, 87, -9, 8, -10, -45}), 2);
+    testar(maiorTime({2, 99, 87, -9, 8, -10, -45}), {2, 99, 87});
+    testar(semTime({2, 99, 87, -9, 8, -10, -45}), 2);
+}
+
+void testarGrupos() {
+    cout << "\nTestes de grupos: \n";
+    testar(casais({-4, -98, -74, -35, -43, -10, 98, 98, 35, 43}), 3);
+    testar(trios({-2, 2, 40, 99, 2, -40, 98, 40}), 2);
+}
+
+void testarAlteracao() {
+    cout << "\nTestes de alteração: \n";
+    testar(remove({2, 99, 87, -9, 8, -10, -45}, 8), {2, 99, 87, -9, -10, -45});
+    testar(insert({2, 99, 87, -9, -10, -45}, 5, 3), {2, 99, 87, 5, -9, -10, -45});
+    testar(dance({3, 4, -4, -3, 5}), {5});
+}
+
 int main() {
+    srand(time(NULL));
+
     testarBusca();
     testarMelhorCaso();
     testarContagem();
     testarFilter();
     testarAcesso();
     testarConjuntos();
+    testarManipulacao();
+    testarProximidade();
+    testarSequencias();
+    testarGrupos();
+    testarAlteracao();
 }
