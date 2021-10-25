f7581c381f316a976d30c41f3fde769029bd0538
Antônio Hugo <hugorplobo@gmail.com>
2021-10-24 23:45:20 -0300

    Add background music, update events and add menu screen

--- digitacao/game/game.cpp
+++ digitacao/game/game.cpp
@@ -3,10 +3,20 @@
 #define WINDOW_W 800
 #define WINDOW_H 600
 
-Game::Game() : window(sf::VideoMode(WINDOW_W, WINDOW_H), "Typing"), board{window} {
+Game::Game() : window(sf::VideoMode(WINDOW_W, WINDOW_H), "Typing"), board{window, player}, menu {window, player} {
+    if (!background_music.openFromFile("assets/sounds/lofi.ogg")) {
+        std::cout << "Failed to open lofi.ogg\n";
+        exit(1);
+    }
+
+    background_music.setLoop(true);
+    background_music.setVolume(20);
+    background_music.play();
+
     on_update = [&]() {
         gameplay();
     };
+
     window.setFramerateLimit(60);
 }
 
@@ -15,30 +25,38 @@ void Game::process_events() {
     while (window.pollEvent(event)) {
         if (event.type == sf::Event::Closed) {
             window.close();
+        } else if (event.type == sf::Event::KeyPressed && event.key.code == sf::Keyboard::Escape) {
+            paused = !paused;
+            if (paused) {
+                on_update = [&]() { menu_screen(); };
+            } else {
+                on_update = [&]() { gameplay(); };
+            }
+        } else if (event.type == sf::Event::TextEntered && !paused) {
+            char code = static_cast<char>(event.text.unicode);
+            code = toupper(code);
+            if (int(code) != 27) {
+                board.mark_by_hit(code);
+            }
         } else if (event.type == sf::Event::TextEntered) {
             char code = static_cast<char>(event.text.unicode);
             code = toupper(code);
-            board.mark_by_hit(code);
+            menu.handle_input(code);
         }
     }
 }
 
 void Game::gameplay() {
     board.update();
+    player.update();
     window.clear();
     board.draw();
     window.display();
-    if (board.misses > 10) {
-        on_update = [&]() {
-            gameover();
-        };
-    }
 }
 
-void Game::gameover() {
-    static Pencil pencil(window);
-    window.clear(sf::Color::Red);
-    pencil.write("Game Over", 400, 300, 50, sf::Color::White);
+void Game::menu_screen() {
+    window.clear();
+    menu.draw();
     window.display();
 }
 
--- digitacao/game/game.hpp
+++ digitacao/game/game.hpp
@@ -1,21 +1,28 @@
 #pragma once
 
 #include <SFML/Graphics.hpp>
+#include <SFML/Audio.hpp>
 #include <vector>
 #include <functional>
 #include "../board/board.hpp"
 #include "../pencil/pencil.hpp"
+#include "../player/player.hpp"
+#include "../menu/menu.hpp"
 
 class Game {
     sf::RenderWindow window;
+    sf::Music background_music;
+    Player player;
     Board board;
+    Menu menu;
     std::function<void()> on_update;
+    bool paused {false};
 
 public:
     Game();
 
     void process_events();
     void gameplay();
-    void gameover();
+    void menu_screen();
     void main_loop();
 };
