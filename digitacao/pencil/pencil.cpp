434404abbde2293ea2950ba98ef245e190adeb98
Antônio Hugo <hugorplobo@gmail.com>
2021-10-24 23:43:52 -0300

    Update font in pencil class

--- digitacao/pencil/pencil.cpp
+++ digitacao/pencil/pencil.cpp
@@ -1,7 +1,7 @@
 #include "pencil.hpp"
 
 Pencil::Pencil(sf::RenderWindow& window) : window {window} {
-    if (!font.loadFromFile("assets/fonts/superstar.ttf")) {
+    if (!font.loadFromFile("assets/fonts/LEMONMILK-Regular.otf")) {
         std::cout << "Failed to load superstar font!" << std::endl;
         exit(1);
     }
